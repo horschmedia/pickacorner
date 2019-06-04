003f76018211a4d1ad18d48e2787e04345995742
horschmedia <dan@horschmedia.com>
2019-06-04 13:50:47 -0500

    added base html and corners grid

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="author" content="name">
+    <meta name="description" content="description here">
+    <meta name="keywords" content="keywords,here">
+    <title>Pick A Corner</title> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha256-YLGeXaapI0/5IgZopewRJcFXomhRMlYYjugPLSyNjTY=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+  </head>
+  <body>
+    <div class="main-container">
+      <div class="row">
+        <div id="corner-1" class="col-6 corner">
+          <p>1</p>
+        </div>
+        <div id="corner-2" class="col-6 corner">
+          <p>2</p>
+        </div>
+        <div id="corner-3" class="col-6 corner">
+          <p>3</p>
+        </div>
+        <div id="corner-4" class="col-6 corner">
+          <p>4</p>
+        </div>
+      </div>
+    </div>
+    <h1 id="title">Pick A Corner</h1>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha256-CjSoeELFOcH0/uxWu6mC/Vlrc1AARqbm/jiiImDGV3s=" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
