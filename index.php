7d8433943be5c65ca5fcff18ccb05788bd60f57a
horschmedia <dan@horschmedia.com>
2019-06-07 15:16:14 -0500

    added score section, and colored corners

--- index.php
+++ index.php
@@ -6,11 +6,34 @@
     <meta name="author" content="name">
     <meta name="description" content="description here">
     <meta name="keywords" content="keywords,here">
-    <title>Pick A Corner</title> 
+    <title>Pick A Corner</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha256-YLGeXaapI0/5IgZopewRJcFXomhRMlYYjugPLSyNjTY=" crossorigin="anonymous" />
     <link rel="stylesheet" href="css/styles.css" type="text/css">
   </head>
   <body>
+    <div class="top-container">
+      <div class="score-container">
+        <div class="row">
+          <div class="col-6">
+            <div class="current-score">
+              100
+            </div>
+            <div class="current-score-label">
+              Your Score
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="high-score">
+              1900
+            </div>
+            <div class="high-score-label">
+              High Score
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="main-container">
       <div class="row">
         <div id="corner-1" class="col-6 corner">
@@ -27,7 +50,6 @@
         </div>
       </div>
     </div>
-    <h1 id="title">Pick A Corner</h1>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha256-CjSoeELFOcH0/uxWu6mC/Vlrc1AARqbm/jiiImDGV3s=" crossorigin="anonymous"></script>
