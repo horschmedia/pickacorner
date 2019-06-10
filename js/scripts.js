7b64eff0aa3a490b1616de2121565537c94d0ab7
horschmedia <dan@horschmedia.com>
2019-06-10 11:49:55 -0500

    added random number selector function

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,45 @@
 // var spin = setInterval(function() {
 //
 // }, 1000);
+var timer;
 
-var endDegree = 1750;
-var currentDegree = 0;
-var change = 5;
-var step = 2;
-spin();
+$('h1').on("click", function() {
+  var currentDegree = 0;
+  var change = 5;
+  var step = 2;
+  clearTimeout(timer);
+  var endDegree = getRandomNumber();
+  console.log(endDegree);
+  spin();
 
-function spin() {
-  if(currentDegree != endDegree) {
-    $('#hand-spinner').css({'transform': 'rotate(' + currentDegree + 'deg)'});
+  function spin() {
+    currentDegree += step;
+    if(currentDegree <= endDegree) {
+      $('#hand-spinner').css({'transform': 'rotate(' + currentDegree + 'deg)'});
+    }
+
+    change = (endDegree / (endDegree - currentDegree));
+
+    timer = setTimeout(function() {
+      if(currentDegree >= endDegree) {
+        clearTimeout(timer);
+        return;
+      }
+      else {
+        spin();
+      }
+    }, change);
   }
+});
 
-  change = endDegree / (endDegree - currentDegree);
+var min = 1080;
+var max = 1440;
+var number;
 
-  timer = setTimeout(function() {
-    if(currentDegree == endDegree) {
-      clearInterval(timer);
-      return;
-    }
-    else {
-      currentDegree += step;
-      spin();
-    }
-  }, change);
+function getRandomNumber() {
+  number = Math.floor(Math.random() * (max-min) + min);
+  while(!(number % 90)) {
+    number = Math.floor(Math.random() * (max-min) + min);
+  }
+  return number;
 }
